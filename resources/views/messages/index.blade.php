4a66148d2718c4764d1ee84d3f40aaf7806e126d
vagrant <vagrant@homestead>
2019-05-07 14:21:06 +0000

    add function of image_upload 2

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -15,7 +15,7 @@
                         <div style='padding-left:10px;text-align:left'>コメント数：<span style='color:white;font-weight:bold;'>{{ $message['count_comments'] }}</span></div>
                         <div style='padding-left:10px;text-align:left'>閲覧数：<span style='color:white;font-weight:bold;'>{{ $message['count_views'] }}</span></div>
                     </time>
-                    <?php if($message['image_name']==''){ $message['image_name']='science.jpg'; } ?>
+                    <?php $message['image_name']='science.jpg'; ?>
                     <img class="image" src="{{ asset('storage/img/' .  $message['image_name']) }}" width="100px" height="100px" alt="コーディング画面">
                 </a>
             @endforeach
